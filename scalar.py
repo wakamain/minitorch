2954870b15ec7baed49890f9a78ac390cb932118
Peter Wakaruma <peter.w.mbiyu@gmail.com>
2024-05-18 20:42:12 +0300

    Another dunder in-built method is add, responsible for addition. It is called when adding two object using "+". Take a look at it for a while and try to understand the implementation.

--- scalar.py
+++ scalar.py
@@ -5,13 +5,20 @@ import numpy as np
 # typing for better type annotation
 from typing import Union, Sequence, Tuple
 
-# Scalar Class (An object that contains a value e.g an int or float)
+# Scalar Class (An object that contains a singular value e.g an int or float)
+# A scalar is a single number that represents a quantity, having magnitude but no direction.
 class Scalar:
   def __init__(self, value):
     self.value = value
 
+  # https://docs.python.org/3/library/functions.html#repr
   def __repr__(self):
     return f"{self.value}"
-
-A = Scalar(1.0)
-print(A)
+  
+  def __add__(self, other):
+    return Scalar(self.value + other.value)
+  
+A = Scalar(1)
+B = Scalar(2)
+C = A + B
+print(C)
